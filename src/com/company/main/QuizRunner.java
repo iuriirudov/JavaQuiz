f221f784cf2704dd03077f9fa091bb7fa80c05b0
Iurii Rudov <iuriirudov@gmail.com>
2022-02-08 21:55:47 -0500

    Completed QuizRunner class to run the quiz, implemented test result with passing score > 60

--- src/com/company/main/QuizRunner.java
+++ src/com/company/main/QuizRunner.java
@@ -1,6 +1,7 @@
 package com.company.main;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class QuizRunner {
@@ -8,25 +9,44 @@ public class QuizRunner {
         Scanner in = new Scanner(System.in);
         Question q = new Question();
         Answer a = new Answer();
-        int score = 0;
+        double score = 0;
+        int response = 0;
 
         ArrayList<Question> questions = q.getQuestions();
         ArrayList<Answer> answers = a.getAnswers();
-        int totalQuestions = questions.size();
+        Collections.shuffle(answers);
+        double totalQuestions = questions.size();
 
         System.out.println("Welcome to the Quiz!");
         for (Question question : questions) {
             System.out.println("Question #" + question.getId() + ": " + question.getQuestion() + "\n");
             System.out.println("Answer options: ");
             int i = 1;
+            ArrayList<Answer> specificAnswers = new ArrayList<>();
             for (Answer answer : answers) {
                 if (answer.getQuestion_id() == question.getId()) {
-                    System.out.println(i + ". " + answer.getAnswer());
-                    i++;
+                    specificAnswers.add(answer);
                 }
             }
-            System.out.println();
-        }
 
+            for (Answer e : specificAnswers) {
+                System.out.println(i + ". " + e.getAnswer());
+                i++;
+            }
+            System.out.println("Please type the number of the correct answer from the list above: ");
+            response = in.nextInt();
+            if (question.getAnswer_id() == specificAnswers.get(response - 1).getId()) {
+                score += 1;
+                System.out.println("You answered correct!");
+            }
+        }
+        double percent = score / totalQuestions * 100.0;
+        String result = null;
+        if (percent > 60) {
+            result = "passed";
+        } else {
+            result = "failed";
+        }
+        System.out.println("Your total score is " + percent + "%. You " + result + " this test!");
     }
 }
